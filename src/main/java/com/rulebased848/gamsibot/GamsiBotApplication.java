e63f0f884ea47f034567dbb392a30cc3bd4e8a3b
ruleBased848 <rkdfowns@naver.com>
2022-09-23 17:51:00 +0900

    feat: add the main class and properties file

---
+++ src/main/java/com/rulebased848/gamsibot/GamsiBotApplication.java
@@ -0,0 +1,11 @@
+package com.rulebased848.gamsibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamsiBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GamsiBotApplication.class, args);
+    }
+}
