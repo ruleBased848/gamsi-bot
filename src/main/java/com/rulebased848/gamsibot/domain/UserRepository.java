c445187d4b3d4c844abe49bc725339b0b8efbdaf
ruleBased848 <rkdfowns@naver.com>
2022-10-01 18:00:39 +0900

    feat: add persisted entities and repositories

---
+++ src/main/java/com/rulebased848/gamsibot/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package com.rulebased848.gamsibot.domain;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+}
