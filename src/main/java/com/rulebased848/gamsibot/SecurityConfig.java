e6d99e062736ba9918a35aa5558cd3539c8733b0
ruleBased848 <rkdfowns@naver.com>
2022-10-05 21:22:27 +0900

    refactor!: disable CSRF protection
    BREAKING CHANGE: we don't need CSRF tokens anymore.

--- src/main/java/com/rulebased848/gamsibot/SecurityConfig.java
+++ src/main/java/com/rulebased848/gamsibot/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .sessionManagement().sessionCreationPolicy(STATELESS).and()
             .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
