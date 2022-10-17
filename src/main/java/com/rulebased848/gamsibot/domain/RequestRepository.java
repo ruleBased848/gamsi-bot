0de28e3815231ffa5e955f2ffdb434a1797c88d3
ruleBased848 <rkdfowns@naver.com>
2022-10-18 02:33:17 +0900

    feat: add JPA repository methods

--- src/main/java/com/rulebased848/gamsibot/domain/RequestRepository.java
+++ src/main/java/com/rulebased848/gamsibot/domain/RequestRepository.java
@@ -1,6 +1,17 @@
 package com.rulebased848.gamsibot.domain;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestRepository extends CrudRepository<Request,Long> {
+    void deleteByChannelId(String channelId);
+
+    List<Request> findByChannelIdAndTargetSubscriberCountLessThanEqual(String channelId, long targetSubscriberCount);
+
+    @Query("SELECT MIN(r.targetSubscriberCount) FROM Request r WHERE r.channelId = ?1")
+    Long findMinimumTargetSubscriberCountByChannelId(String channelId);
+
+    @Query("SELECT new com.rulebased848.gamsibot.domain.ChannelIdAndTargetSubscriberCount(r.channelId, MIN(r.targetSubscriberCount)) FROM Request r GROUP BY r.channelId")
+    List<ChannelIdAndTargetSubscriberCount> findAllChannelIdWithMinimumTargetSubscriberCount();
 }
