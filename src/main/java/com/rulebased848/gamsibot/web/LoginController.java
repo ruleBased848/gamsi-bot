12f72fd9c653d490e15f18983d7a904f9263ee8f
ruleBased848 <rkdfowns@naver.com>
2022-12-03 22:53:20 +0900

    refactor: specify the variable type explicitly

--- src/main/java/com/rulebased848/gamsibot/web/LoginController.java
+++ src/main/java/com/rulebased848/gamsibot/web/LoginController.java
@@ -8,6 +8,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
-        var token = new UsernamePasswordAuthenticationToken(
+        Authentication token = new UsernamePasswordAuthenticationToken(
             credentials.getUsername(),
             credentials.getPassword()
         );
